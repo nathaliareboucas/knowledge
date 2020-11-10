4afa3807aafa2c77f9db91cf9be894b7c6810509
Nathália Rebouças <nathreboucas5@gmail.com>
2020-11-10 20:41:55 -0300

    criando api de categorias

--- config/routes.js
+++ config/routes.js
@@ -8,4 +8,15 @@ module.exports = app => {
 		.route('/users/:id')
 		.put(app.api.user.save)
 		.get(app.api.user.getById);
+
+	app
+		.route('/categories')
+		.post(app.api.category.save)
+		.get(app.api.category.get);
+
+	app
+		.route('/categories/:id')
+		.get(app.api.category.getById)
+		.put(app.api.category.save)
+		.delete(app.api.category.remove);
 };
